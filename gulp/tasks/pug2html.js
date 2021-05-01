308daf891acceddd1246158e4e987f8d685af796
Dendibaev Arman <brunoalex2k19@gmail.com>
2021-05-02 02:13:16 +0300

    fix: validators

--- gulp/tasks/pug2html.js
+++ gulp/tasks/pug2html.js
@@ -1,6 +1,15 @@
 const gulp = require("gulp");
+const plumber = require("gulp-plumber");
 const pug = require("gulp-pug");
+const pugLinter = require("gulp-pug-linter");
+const bemValidator = require("gulp-html-bem-validator");
 
-module.exports = function pug2html() {
-  return gulp.src("src/pages/*.pug").pipe(pug()).pipe(gulp.dest("build"));
+module.exports = function pug2html(cb) {
+  return gulp
+    .src("src/pages/*.pug")
+    .pipe(plumber())
+    .pipe(pugLinter({ reporter: "default" }))
+    .pipe(pug())
+    .pipe(bemValidator())
+    .pipe(gulp.dest("build"));
 };
